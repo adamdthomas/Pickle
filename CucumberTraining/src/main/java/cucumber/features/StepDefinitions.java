35597aeec2d011c79c85764513cb55e9e44c0020
Adam THomas <adamdthomas@gmail.com>
2017-10-08 10:33:12 -0400

    Adding steps class

---
+++ CucumberTraining/src/main/java/cucumber/features/StepDefinitions.java
@@ -0,0 +1,28 @@
+package cucumber.features;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.*;
+
+
+public class StepDefinitions {
+	
+	
+	@Given("^this is my first test$")
+	public void this_is_my_first_test() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+	@When("^I am running my first test$")
+	public void i_am_running_my_first_test() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+	@Then("^My test should run$")
+	public void my_test_should_run() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+}
